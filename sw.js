792e0a15f3af98d5c1fd75fe1b0f966740ebc902
Rahul Naskar <142179532+rahul4746@users.noreply.github.com>
2026-02-01 16:53:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Service Worker
    =============================== */
 
-const CACHE_NAME = "sastafy-v1.3";
+const CACHE_NAME = "sastafy-v1.4";
 
 const FILES_TO_CACHE = [
   "./",                // root
@@ -70,6 +70,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
