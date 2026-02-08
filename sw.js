3fa6c6f0c41a2bb492262b0e24c8e612d2213e54
Rahul Naskar <142179532+rahul4746@users.noreply.github.com>
2026-02-08 20:09:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Service Worker
    =============================== */
 
-const CACHE_NAME = "beatflow-v1.9"; // ⬅️ bump version to force update
+const CACHE_NAME = "beatflow-v1.10"; // ⬅️ bump version to force update
 
 const FILES_TO_CACHE = [
   "./",                       // root
@@ -83,5 +83,6 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
